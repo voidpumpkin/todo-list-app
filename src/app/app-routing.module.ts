4cbe2cda9bf7b58f892bfe11d51c324c807b7a19
Julius Lungys <juliuslungys@gmail.com>
2018-12-12 03:28:56 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TodoListComponent} from './todo-list/todo-list.component'
+import { ArchivedTodoListComponent} from './archived-todo-list/archived-todo-list.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoListComponent
+  },
+  {
+    path: 'archive',
+    component: ArchivedTodoListComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
